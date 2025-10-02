4c8a548a89865ffad9ff7e80b240962878a5e28f
learntech22 <mich22engineer@gmail.com>
2025-10-02 14:27:13 +0200

    Add second EC2 instance for demo

--- main.tf
+++ main.tf
@@ -3,10 +3,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-052064a798f08f0d3"  # Amazon Linux 2 in us-east-1
+  ami           = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraform-demo-instance"
+    Name = "terraform-demo-instance-1"
+  }
+}
+
+# New EC2 instance
+resource "aws_instance" "demo2" {
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-demo-instance-2"
   }
 }
